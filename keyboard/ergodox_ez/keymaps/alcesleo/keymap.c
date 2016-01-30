6a0d88a66f06b8b4a97e1e021ec862de04ed95a4
Jimmy Börjesson <alcesleo@gmail.com>
2016-01-30 11:55:55 +0100

    Add middle mouse button

--- keyboard/ergodox_ez/keymaps/alcesleo/keymap.c
+++ keyboard/ergodox_ez/keymaps/alcesleo/keymap.c
@@ -137,7 +137,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 
-        KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_BTN3,
         KC_TRNS,
         KC_TRNS, KC_BTN2, KC_BTN1
     )
