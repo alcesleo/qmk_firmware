f6baf343b9d4e6c736608a89be31f8596d2c194a
Jimmy Börjesson <alcesleo@gmail.com>
2016-03-17 12:03:30 +0100

    Improve layout

--- keyboard/ergodox_ez/keymaps/alcesleo/keymap.c
+++ keyboard/ergodox_ez/keymaps/alcesleo/keymap.c
@@ -13,10 +13,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |        |  1   |  2   |  3   |  4   |  5   |  -   |           |  +   |  6   |  7   |  8   |  9   |  0   |        |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |  Tab   |  Q   |  W   |  E   |  R   |  T   | Tab  |           |Enter |  Y   |  U   |  I   |  O   |  P   | MOUSE  |
+ * |  Tab   |  Q   |  W   |  E   |  R   |  T   |Media |           |Mouse |  Y   |  U   |  I   |  O   |  P   |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |  A   |  S   |  D   |  F   |  G   |------|           |------|  H   |  J   |  K   |  L   |  ;:  |   '    |
- * |--------+------+------+------+------+------| Home |           | End  |------+------+------+------+------+--------|
+ * |  Ctrl  |  A   |  S   |  D   |  F   |  G   |------|           |------|  H   |  J   |  K   |  L   |  ;:  |   '    |
+ * |--------+------+------+------+------+------| Cmd  |           | Cmd  |------+------+------+------+------+--------|
  * | LShift |  Z   |  X   |  C   |  V   |  B   |      |           |      |  N   |  M   |  ,<  |  .>  |  /?  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |      |      |      | LEFT |RIGHT |                                       | DOWN |  UP  |      |      |      |
@@ -24,29 +24,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                       ,----------------.   ,----------------.
  *                                       |Ctrl/Esc|Alt/Tab|   |       |Ctrl/Esc|
  *                                ,------|--------|-------|   |-------+--------+------.
- *                                |      |        |  Cmd  |   |  Cmd  |        |      |
- *                                |Bkspc | SYMBOL |-------|   |-------| Enter  |Space |
+ *                                |      |        |       |   |       |        |      |
+ *                                |Symbol| Bkspc  |-------|   |-------| Enter  |Space |
  *                                |      |        | MEDIA |   |       |        |      |
  *                                `-----------------------'   `-----------------------'
  */
 [DEFAULT] = KEYMAP(
         // Left hand
         KC_NO,    KC_1,     KC_2,    KC_3,    KC_4,    KC_5, KC_MINUS,
-        KC_TAB,   KC_Q,     KC_W,    KC_E,    KC_R,    KC_T, KC_TAB,
-        KC_BSPC,  KC_A,     KC_S,    KC_D,    KC_F,    KC_G,
-        KC_LSFT,  KC_Z,     KC_X,    KC_C,    KC_V,    KC_B, KC_HOME,
+        KC_TAB,   KC_Q,     KC_W,    KC_E,    KC_R,    KC_T, MO(MEDIA),
+        KC_LCTL,  KC_A,     KC_S,    KC_D,    KC_F,    KC_G,
+        KC_LSFT,  KC_Z,     KC_X,    KC_C,    KC_V,    KC_B, KC_LGUI,
         KC_NO,    KC_NO,    KC_NO,   KC_LEFT, KC_RGHT,
 
                                         CTL_T(KC_ESC), ALT_T(KC_TAB),
                                                              KC_LGUI,
-                                      KC_BSPC, MO(SYMBOL), MO(MEDIA),
+                                      MO(SYMBOL), KC_BSPC, MO(MEDIA),
 
         // Right hand
-        KC_PLUS,  KC_6,    KC_7,    KC_8,    KC_9,     KC_0,    KC_NO,
-        KC_ENTER, KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,    TG(MOUSE),
-                  KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN, KC_QUOT,
-        KC_END,   KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH, KC_RSFT,
-                           KC_DOWN, KC_UP,   KC_NO,    KC_NO,   KC_NO,
+        KC_PLUS,   KC_6,    KC_7,    KC_8,    KC_9,     KC_0,    KC_NO,
+        TG(MOUSE), KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,    KC_BSLS,
+                   KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN, KC_QUOT,
+        KC_RGUI,   KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH, KC_RSFT,
+                            KC_DOWN, KC_UP,   KC_NO,    KC_NO,   KC_NO,
 
         KC_NO, CTL_T(KC_ESC),
         KC_RGUI,
