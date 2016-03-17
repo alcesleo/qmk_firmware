6aadb4ba92c27096ad6c071a1888705e283b0f44
Jimmy Börjesson <alcesleo@gmail.com>
2016-03-17 12:10:40 +0100

    Add inverted scrolling to mouse layer

--- keyboard/ergodox_ez/keymaps/alcesleo/keymap.c
+++ keyboard/ergodox_ez/keymaps/alcesleo/keymap.c
@@ -107,7 +107,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *  |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  *  |        |      |WLEFT |WDOWN |WRIGHT|      |------|           |------|      | LEFT | DOWN |RIGHT |      |        |
  *  |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- *  |        |      |      |      |WFULL |      |      |           |      |      |DBLCLK|      |      |      |        |
+ *  |        |      |      |      |WFULL |      |      |           |      |      |      |      |      |      |        |
  *  `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *    |      |      |      |      |      |                                       |      |      |      |      |      |
  *    `----------------------------------'                                       `----------------------------------'
@@ -133,14 +133,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
         // Right hand
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_U, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_WH_R, KC_MS_U, KC_WH_L, KC_TRNS, KC_TRNS,
                  KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 
         KC_TRNS, KC_BTN3,
-        KC_TRNS,
-        KC_TRNS, KC_BTN2, KC_BTN1
+        KC_WH_D,
+        KC_WH_U, KC_BTN2, KC_BTN1
     ),
 
 /* Keymap 3: Media/arrow layer
