d5e2dcc28c42aecf1625ac10714db54309974820
Jimmy Börjesson <alcesleo@gmail.com>
2016-03-18 12:23:54 +0100

    Activate the LED for layer 3 too

--- keyboard/ergodox_ez/keymaps/alcesleo/keymap.c
+++ keyboard/ergodox_ez/keymaps/alcesleo/keymap.c
@@ -214,7 +214,6 @@ void * matrix_init_user(void) {
 
 // Runs constantly in the background, in a loop.
 void * matrix_scan_user(void) {
-
     uint8_t layer = biton32(layer_state);
 
     ergodox_board_led_off();
@@ -222,16 +221,17 @@ void * matrix_scan_user(void) {
     ergodox_right_led_2_off();
     ergodox_right_led_3_off();
     switch (layer) {
-      // TODO: Make this relevant to the ErgoDox EZ.
         case 1:
             ergodox_right_led_1_on();
             break;
         case 2:
             ergodox_right_led_2_on();
             break;
+        case 3:
+            ergodox_right_led_3_on();
+            break;
         default:
             // none
             break;
     }
-
 };
